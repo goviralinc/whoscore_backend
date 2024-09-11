db9c6f8c4281a71fc39cf73fc313cb9236cd16eb
deji-404 <ayodejiosayemi928@gmail.com>
2024-09-11 08:57:51 +0100

    swagger config

--- src/utils/swagger-setup.ts
+++ src/utils/swagger-setup.ts
@@ -11,13 +11,8 @@ export class SwaggerSetup {
       .setTitle('Whoscore documentation')
       .setDescription('Whoscore documentation')
       .addServer('http://localhost:3000', 'Local Server') // You can add multiple servers
-      .addServer('http://localhost:3001', 'Local Server')
       .addServer('https://whoscore-backend-h8y0.onrender.com', 'Test Server') // You can add multiple servers
-      .addServer(
-        'http://ec2-3-86-184-89.compute-1.amazonaws.com:3000',
-        'dev Server',
-      )
-      .addServer('https://devapi.whoscore.africa', 'Production Server') // Set the addServer value
+      .addServer('https://devapi.whoscore.uk', 'Production Server') // Set the addServer value
       .addBearerAuth()
       .setVersion('1.0')
       .build();
